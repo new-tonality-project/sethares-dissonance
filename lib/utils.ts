2236344b47ad6a07815a6ecdb6cc16f65fe7d244
Seva Deriushkin <seva.deriushkin@gmail.com>
2025-07-25 12:09:38 +0200

    improved loudness calculation

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,14 +3,22 @@ export type Spectrum = {
   loudness: number;
 }[];
 
-/** The formula to calculate loudness from amplitude proposed by Sethares in the appendix "How to Draw Dissonance Curves" */
-export function getSetharesLoudness(amplitude: number): number {
-  const P_e = amplitude / Math.SQRT2;
-  const P_ref = 20;
+const NORMALISATION_PRESSURE_UNIT = 2.8284271247461905;
 
-  const SPL = 20 * Math.log10(P_e / P_ref);
+/**
+ * The formula to calculate loudness from amplitude. The converstion to SPL (phons) is done according to Sethares in the appendix "How to Draw Dissonance Curves". The converstion to loudness is done according to https://sengpielaudio.com/calculatorSonephon.htm
+ * @param {number} amplitude - the normalized peak value where 0.001 corresponds to SPL of 40 db or 1 sone and 1 to SPL of 100db and 64 sones. Normalisation is done for the simplicity of providing harmonic spectrum with amplitudes 1, 1/2, 1/3, ...
+ */
+export function getLoudness(amplitude: number): number {
+  const rms = amplitude / Math.SQRT2;
+  const pressure = rms * NORMALISATION_PRESSURE_UNIT;
+  const referencePressure = 2e-5;
 
-  return 2 ** (SPL / 10) / 16;
+  const phons = 20 * Math.log10(pressure / referencePressure);
+
+  if (phons < 8) return 0;
+  if (phons < 40) return Math.pow(phons / 40, 2.86) - 0.005;
+  return Math.pow(2, (phons - 40) / 10);
 }
 
 /** Fitting parameters proposed by Sethares in the appendix "How to Draw Dissonance Curves" */
